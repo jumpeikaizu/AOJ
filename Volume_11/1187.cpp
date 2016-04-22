933d911537da7eabc1ac5a1ba289faa2a68f6bce
Jumpei Kaizu <jumpei@MacBook-Air.local>
2016-04-22 23:12:18 +0900

    1187

--- Volume_11/1187.cpp
+++ Volume_11/1187.cpp
@@ -2,3 +2,23 @@
 #include<vector>
 using namespace std;
 
+struct Team{
+  int id,time,ac_num;
+  vector<bool> ac;
+  vector<int> wa;
+  Team(int P,int _id){
+	id = _id;
+	time = 0;
+	ac_num = 0;
+	ac = vector<bool>(P,false);
+	wa = vector<int>(P,0);
+  }
+  bool operator<(const Team &r) const{
+	if(ac_num == r.ac_num){
+	  ac_num < r.ac_num;
+	}
+	else{
+	  return ac < r.ac;
+	}
+  
+  

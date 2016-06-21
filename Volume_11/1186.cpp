b28a3b25b3af2e524d4fd486e170b3b025090d35
kaizu <jumpei_kaizu@shino.ecei.tohoku.ac.jp>
2016-06-22 04:03:00 +0900

    1186

--- Volume_11/1186.cpp
+++ Volume_11/1186.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
+#include<map>
+#include<vector>
+#include<algorithm>
 using namespace std;
-
+typedef pair<int,int> P;
 int main(){
   int h,w;
-  while(cin >> h >> w){
-    if(h==0&&w==0) break;
-    int d=h*h+w*w;
-    int ans_d=151*151*2;
-    int ans_h=151;
-    int ans_w;
-    for(int ih=1;ih<=150;ih++){
-      for(int iw=ih+1;iw<=150;iw++){
-	int id=ih*ih+iw*iw;
-	if(d>id || (d==id && h>=ih)) continue;
-	if(ans_d>id || (ans_d==id && ih<ans_h)){
-	  ans_h = ih;
-	  ans_w = iw;
-	  ans_d = id;
+  vector<pair<P,int> > v;
+  
+  for(h=1;h<=150;h++)
+	for(w=h+1;w<=150;w++)
+	  v.push_back(pair<P,int>(P(h*h+w*w,h),w));
+  sort(v.begin(),v.end());
+  
+  while(cin>>h>>w, h||w){
+	pair<P,int> p(P(h*h+w*w,h),w);
+	for(int i=0;i<v.size();i++){
+	  if(v[i]>p){
+		cout<<v[i].first.second<<' '<<v[i].second<<endl;
+		break;
+	  }
 	}
-      }
-    }
-    cout<<ans_h<<" "<<ans_w<<endl;
   }
+  return 0;
 }
 
